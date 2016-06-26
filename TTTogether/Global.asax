24014155e75dd0847a522994417b090e68c7f5f4
Chenru <Chenru@OFFICE-PC>
2016-06-26 00:19:39 -0600

    Add project files.

---
+++ TTTogether/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TTTogether.MvcApplication" Language="C#" %>
---
+++ TTTogether/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TTTogether.Startup))]
+namespace TTTogether
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
